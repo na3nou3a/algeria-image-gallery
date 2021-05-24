f1bb049fa1cb29978a8d18216695d17ae78dfe0f
naima1515 <mirnanina2014@gmail.com>
2021-05-24 20:54:45 +0100

    change images format to jpp and some performance to .js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,39 @@
-window.onload = prepareLinks;
+function addLoadEvent(func) {
+  let oldonload = window.onload;
+  if (typeof window.onload != "function") {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldonload();
+      func();
+    };
+  }
+}
+function insertAfter(newElement, targetElement) {
+  let parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("gallery")) return false;
+  let placeholder = document.createElement("img");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/algeria/placeHolder.jpg");
+  placeholder.setAttribute("alt", "my image gallery");
+  let description = document.createElement("p");
+  description.setAttribute("id", "description");
+  let desctext = document.createTextNode("Choose an image to preview");
+  description.appendChild(desctext);
+  let gallery = document.getElementById("gallery");
+  insertAfter(placeholder, gallery);
+  insertAfter(description, placeholder);
+}
 function prepareLinks() {
   if (!document.getElementsByTagName || !document.getElementById) {
     return false;
@@ -30,9 +65,10 @@ function showPic(elem) {
     text = elem.getAttribute("title") ? elem.getAttribute("title") : "";
     description = document.getElementById("description");
     if (description.firstChild.nodeType == 3) {
-      //text node
       description.firstChild.nodeValue = text;
     }
   }
   return true;
 }
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareLinks);
